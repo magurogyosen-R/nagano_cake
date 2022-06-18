873fa0ee5ced668f6128db0a07ccbe8827b29358
EC2 Default User <104186291+t-yuusuke@users.noreply.github.com>
2022-06-18 08:05:52 +0000

    Add Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_18_042921) do
+ActiveRecord::Schema.define(version: 2022_06_18_073513) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "postcode"
+    t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
