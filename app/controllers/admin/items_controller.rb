604267ddc254bf8544f48e1791e1f3dad92620e1
EC2 Default User <ec2-user@ip-172-31-24-129.ec2.internal>
2022-06-20 03:24:56 +0000

    Edit items new index show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,27 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to '/admin/items'
   end
 
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :image, :price)
+  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'noimage.jpeg'
+    end
+  end
 end
