115b159440fd4dfbedff49ae592786b39d1fdcd6
EC2 Default User <ec2-user@ip-172-31-24-129.ec2.internal>
2022-06-21 02:42:44 +0000

    Edit item genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,24 @@
 class Admin::GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to '/admin/genres'
+  end
+
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
   end
 end
+
+private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,10 +18,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
 end
 
 private
   def item_params
-    params.require(:item).permit(:name, :introduction, :image, :price)
+    params.require(:item).permit(:name, :introduction, :image, :price, :id)
   end
