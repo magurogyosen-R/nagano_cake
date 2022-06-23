516ca83e3c03caab7583873daa3a5f89b0776616
EC2 Default User <ec2-user@ip-172-31-24-129.ec2.internal>
2022-06-23 03:16:58 +0000

    編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,19 +1,26 @@
 class Public::CartItemsController < ApplicationController
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    item.save
+    redirect_to cart_item_path(@cart_item.id)
+  end
+
   def index
     @order = Order.new
+    @cart_items = CartItem.all
   end
 
   def destroy
-    cart_item = Cart_Item.find(params[:id])
+    cart_item = CartItem.find(params[:id])
     cart_item.destroy
   end
 
   def destroy_all
-    cart_item = Cart_Item.where(cart_item)
+    cart_item = CartItem.where(cart_item)
     cart_item.destroy_all
   end
 
-  privete
+  private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
